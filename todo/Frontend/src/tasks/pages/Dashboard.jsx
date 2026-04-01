dd1038a2a33ede9757e2cac669bbf101adf6c783
trushitdarji <trushitdarji55@gmail.com>
2026-04-01 20:20:22 +0530

    enhance Dashboard component with date display and task overview

--- todo/Frontend/src/tasks/pages/Dashboard.jsx
+++ todo/Frontend/src/tasks/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
 import { useTask } from "../hook/useTask";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { data, Link } from "react-router-dom";
 
 const Dashboard = () => {
+  const [currunetDate, setcurrunetDate] = useState(new Date());
+
   const { handleGetAllNotes, loading, tasks } = useTask();
   useEffect(() => {
     handleGetAllNotes();
@@ -12,8 +14,8 @@ const Dashboard = () => {
     <>
       <header>
         <div
-          style={{ paddingInline: "2rem", paddingBlock:"0.8rem" }}
-          className="nav flex justify-between text-lg bg-blue-600"
+          style={{ paddingInline: "2rem", paddingBlock: "0.8rem" }}
+          className="nav flex justify-between text-lg border-b-1   border-b-zinc-700"
         >
           <div className="left">
             <div className="logo">
@@ -27,10 +29,69 @@ const Dashboard = () => {
         </div>
       </header>
 
-    <section>
-      
-    </section>
+      <section
+        className="main flex flex-col gap-20"
+        style={{ marginInline: "10%", marginBlock: "5rem" }}
+      >
+        <section className="first flex flex-col gap-3.5">
+          <div className="top">
+            <div className="dates">
+              <h1 className="font-bold text-5xl">
+                {currunetDate.toDateString()}
+              </h1>
+            </div>
+          </div>
+          <div className="bottom flex  gap-3">
+            <div className="left">
+              <p
+                className="bg-zinc-600 w-fit text-zinc-300 rounded-xl "
+                style={{ paddingInline: "1rem", fontSize: "0.8rem" }}
+              >
+                Tasks Remaining
+              </p>
+            </div>
+            <div>
+              <p
+                className="bg-green-800 w-fit text-green-500 rounded-xl"
+                style={{ paddingInline: "0.6rem", fontSize: "0.8rem" }}
+              >
+                Focused Mode
+              </p>
+            </div>
+          </div>
+        </section>
 
+        <section className="second flex gap-8">
+          <div className="left">
+            <div className="quats">
+              <h1
+                className="font-bold text-xl max-w-56 bg-zinc-800 rounded-xl"
+                style={{
+                  paddingInline: "1rem",
+                  paddingBlock: "1rem",
+                  minWidth: "16rem",
+                  minHeight: "20rem",
+                }}
+              >
+                "Every Day Is A Chanse To Be Better"
+              </h1>
+            </div>
+          </div>
+          <div className="right  w-full">
+            <div className="all-tasks bg-zinc-800 w-full rounded-lg">
+              <div className="task1 "
+              style={{paddingInline:"1rem",paddingBlock:"0.3rem"}}>
+                <div className="subject">
+                  <h3 className="font-bold text-xl ">Subject 1</h3>
+                </div>
+                <div className="task">
+                  <p>here is your first task 1</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
     </>
   );
 };
